9a297fa83e503731ee20ca4d5d156f5beb98b328
ShrooqAlsadiq <157583573+ShrooqAlsadiq@users.noreply.github.com>
2025-05-03 17:19:00 +0300

    comments on index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Find the DOM element with id="root" and create a React root for it
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component tree inside the React.StrictMode wrapper
 root.render(
   <React.StrictMode>
     <App />
